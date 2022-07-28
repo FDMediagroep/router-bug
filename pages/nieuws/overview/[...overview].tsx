4431b84824d2d79d798609799f7cdcc16cc1e7b0
Willem Liu <willem.liu@fdmediagroep.nl>
2022-07-28 08:42:51 +0200

    v12.2.4-canary.3

--- pages/nieuws/overview/[...overview].tsx
+++ pages/nieuws/overview/[...overview].tsx
@@ -1,12 +1,43 @@
-import type { GetStaticPaths, GetStaticProps } from "next";
+import type {
+  GetStaticPaths,
+  GetStaticProps,
+  GetStaticPropsContext,
+} from "next";
+import Link from "next/link";
 
-function Page() {
-  return <h1>Test page</h1>;
+interface Props {
+  page: number;
 }
 
-export async function getStaticProps(): Promise<ReturnType<GetStaticProps>> {
+function Page(props: Props) {
+  const previous = props?.page - 1;
+  const next = props?.page + 1;
+  return (
+    <>
+      <h1>Test page {props.page}</h1>
+      <nav>
+        {previous > 0 && (
+          <Link href={`/nieuws?page=${previous}`} passHref={true}>
+            <a style={{ marginRight: "1rem" }}>Page {previous}</a>
+          </Link>
+        )}
+        <Link href={`/nieuws?page=${next}`} passHref={true}>
+          <a>Page {next}</a>
+        </Link>
+      </nav>
+    </>
+  );
+}
+
+export async function getStaticProps(
+  ctx: GetStaticPropsContext
+): Promise<ReturnType<GetStaticProps>> {
   return {
-    props: {},
+    props: {
+      page: ctx.params?.overview
+        ? parseInt(ctx.params?.overview as string, 10)
+        : 1,
+    },
   };
 }
 
